0414d90531315e369309cd944a9d0714d8375f27
shedatario <44762867+shedatario@users.noreply.github.com>
2019-01-26 03:08:08 +0800

    Add files via upload

--- app/views/login.php
+++ app/views/login.php
@@ -4,28 +4,37 @@
 <?php function get_page_content() {  ?>
 
 	<div class="container"> <!-- start container -->
-		<div class="jumbotron bg-dark text-light text-center mt-5">
-			<h4 class="text-center">Login</h4>
-		</div> <!-- end jumbotron -->
-		
-		<form>
-			<div class="form-group">
-				<label for = "username"> Username: </label>
-				<input type="text" id = "username" name="username" class="form-control" placeholder="Enter username">
-				<span class="validation"></span>
-			</div>
+		<div class="row">
+			<div class="col-sm-8">
+				
+				<div class="card text-center" style="width: 30rem;">
+				 	<img class="card-img-top" src="../assets/images/login.jpeg" alt="Card image cap" height="50%" width="50%">
+					<div class="card-body">
+						<form>
+							<div class="form-group">
+								<label for = "username"></label>
+								<input type="text" id = "username" name="username" class="form-control" placeholder="Enter username">
+								<span class="validation"></span>
+							</div>
 
-			<div class="form-group">
-				<label for="password"> Password: </label>
-				<input type="password" id = "password" name="password" class="form-control" placeholder="Enter password">
-				<span class="validation"></span>
+							<div class="form-group">
+								<label for="password"></label>
+								<input type="password" id = "password" name="password" class="form-control" placeholder="Enter password">
+								<span class="validation"></span>
+							</div>
+						</form>	<!-- end form -->
+					</div>
+					<div class="text-center py-1">
+					<a href="./register.php" class="btn btn-secondary"> Register </a>
+					<button type = "submit" class="btn btn-primary" id="login"> Login </button>	
+				</div>
+				</div>
+				
 			</div>
-		</form>	<!-- end form -->
-
-		<div class="text-center py-4">
-			<a href="./register.php" class="btn btn-secondary"> Register </a>
-			<button type = "submit" class="btn btn-primary" id="login"> Login </button>	
 		</div>
+		
+
+		
 			
 	</div><!--  end container -->
 
